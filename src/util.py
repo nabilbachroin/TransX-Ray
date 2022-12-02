8e43234ae1c7d6956f80653eaff1785458b6ef05
nabilbachroin <abiel769@gmail.com>
2022-12-02 08:05:48 +0800

    bismillah rapi dan berhasil

---
+++ src/util.py
@@ -0,0 +1,181 @@
+import random
+
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import backend as K
+from keras.preprocessing import image
+from sklearn.metrics import roc_auc_score, roc_curve
+from tensorflow.compat.v1.logging import INFO, set_verbosity
+import numpy as np
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.preprocessing import MultiLabelBinarizer
+
+random.seed(a=None, version=2)
+
+set_verbosity(INFO)
+
+
+def check_for_leakage(df1, df2, patient_col):    
+    df1_patients_unique = set(df1[patient_col])
+    df2_patients_unique = set(df2[patient_col])
+    patients_in_both_groups = df1_patients_unique.intersection(df2_patients_unique)
+
+    # leakage contains true if there is patient overlap, otherwise false.
+    if patients_in_both_groups:
+        leakage = True # boolean (true if there is at least 1 patient in both groups)
+    else:
+        leakage = False
+    return leakage
+
+    
+
+def get_mean_std_per_batch(image_path, df, H=320, W=320):
+    sample_data = []
+    for idx, img in enumerate(df.sample(100)["Image Index"].values):
+        # path = image_dir + img
+        sample_data.append(
+            #np.array(image.load_img(image_path, target_size=(H, W))))
+            np.array(tf.keras.utils.load_img(image_path, target_size=(H, W))))
+
+    mean = np.mean(sample_data[0])
+    std = np.std(sample_data[0])
+    return mean, std
+
+
+def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
+    """Load and preprocess image."""
+    img_path = image_dir + img
+    mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
+    #x = image.load_img(img_path, target_size=(H, W))
+    x = tf.keras.utils.load_img(img_path, target_size=(H, W))
+    if preprocess:
+        x -= mean
+        x /= std
+        x = np.expand_dims(x, axis=0)
+    return x
+
+
+def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
+    """GradCAM method for visualizing input saliency."""
+    y_c = input_model.output[0, cls]
+    conv_output = input_model.get_layer(layer_name).output
+    grads = K.gradients(y_c, conv_output)[0]
+
+    gradient_function = K.function([input_model.input], [conv_output, grads])
+
+    output, grads_val = gradient_function([image])
+    output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+    weights = np.mean(grads_val, axis=(0, 1))
+    cam = np.dot(output, weights)
+
+    # Process CAM
+    cam = cv2.resize(cam, (W, H), cv2.INTER_LINEAR)
+    cam = np.maximum(cam, 0)
+    cam = cam / cam.max()
+    return cam
+
+
+def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
+                    layer_name='bn'):
+    preprocessed_input = load_image(img, image_dir, df)
+    predictions = model.predict(preprocessed_input)
+
+    print("Loading original image")
+    plt.figure(figsize=(15, 10))
+    plt.subplot(151)
+    plt.title("Original")
+    plt.axis('off')
+    plt.imshow(load_image(img, image_dir, df, preprocess=False), cmap='gray')
+
+    j = 1
+    for i in range(len(labels)):
+        if labels[i] in selected_labels:
+            print(f"Generating gradcam for class {labels[i]}")
+            gradcam = grad_cam(model, preprocessed_input, i, layer_name)
+            plt.subplot(151 + j)
+            plt.title(f"{labels[i]}: p={predictions[0][i]:.3f}")
+            plt.axis('off')
+            plt.imshow(load_image(img, image_dir, df, preprocess=False),
+                       cmap='gray')
+            plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
+            j += 1
+
+
+def get_roc_curve(labels, predicted_vals, generator):
+    auc_roc_vals = []
+    for i in range(len(labels)):
+        try:
+            gt = generator.labels[:, i]
+            pred = predicted_vals[:, i]
+            auc_roc = roc_auc_score(gt, pred)
+            auc_roc_vals.append(auc_roc)
+            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
+            plt.figure(1, figsize=(10, 10))
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(fpr_rf, tpr_rf,
+                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC curve')
+            plt.legend(loc='best')
+        except:
+            print(
+                f"Error in generating ROC curve for {labels[i]}. "
+                f"Dataset lacks enough examples."
+            )
+    plt.show()
+    return auc_roc_vals
+
+
+
+
+def generate_class_weights(class_series, multi_class=True, one_hot_encoded=False):
+  """
+  Method to generate class weights given a set of multi-class or multi-label labels, both one-hot-encoded or not.
+  Some examples of different formats of class_series and their outputs are:
+    - generate_class_weights(['mango', 'lemon', 'banana', 'mango'], multi_class=True, one_hot_encoded=False)
+    {'banana': 1.3333333333333333, 'lemon': 1.3333333333333333, 'mango': 0.6666666666666666}
+    - generate_class_weights([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 0, 0]], multi_class=True, one_hot_encoded=True)
+    {0: 0.6666666666666666, 1: 1.3333333333333333, 2: 1.3333333333333333}
+    - generate_class_weights([['mango', 'lemon'], ['mango'], ['lemon', 'banana'], ['lemon']], multi_class=False, one_hot_encoded=False)
+    {'banana': 1.3333333333333333, 'lemon': 0.4444444444444444, 'mango': 0.6666666666666666}
+    - generate_class_weights([[0, 1, 1], [0, 0, 1], [1, 1, 0], [0, 1, 0]], multi_class=False, one_hot_encoded=True)
+    {0: 1.3333333333333333, 1: 0.4444444444444444, 2: 0.6666666666666666}
+  The output is a dictionary in the format { class_label: class_weight }. In case the input is one hot encoded, the class_label would be index
+  of appareance of the label when the dataset was processed. 
+  In multi_class this is np.unique(class_series) and in multi-label np.unique(np.concatenate(class_series)).
+  Author: Angel Igareta (angel@igareta.com)
+  """
+  if multi_class:
+    # If class is one hot encoded, transform to categorical labels to use compute_class_weight   
+    if one_hot_encoded:
+      class_series = np.argmax(class_series, axis=1)
+  
+    # Compute class weights with sklearn method
+    class_labels = np.unique(class_series)
+    class_weights = compute_class_weight(class_weight='balanced', classes=class_labels, y=class_series)
+    return dict(zip(class_labels, class_weights))
+  else:
+    # It is neccessary that the multi-label values are one-hot encoded
+    mlb = None
+    if not one_hot_encoded:
+      mlb = MultiLabelBinarizer()
+      class_series = mlb.fit_transform(class_series)
+
+    n_samples = len(class_series)
+    n_classes = len(class_series[0])
+
+    # Count each class frequency
+    class_count = [0] * n_classes
+    for classes in class_series:
+        for index in range(n_classes):
+            if classes[index] != 0:
+                class_count[index] += 1
+    
+    # Compute class weights using balanced method
+    class_weights = [n_samples / (n_classes * freq) if freq > 0 else 1 for freq in class_count]
+    class_labels = range(len(class_weights)) if mlb is None else mlb.classes_
+    return dict(zip(class_labels, class_weights))
